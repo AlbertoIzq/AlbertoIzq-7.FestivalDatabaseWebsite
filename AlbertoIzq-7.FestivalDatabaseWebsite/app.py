4ff0599d961a13d6cfd3432f842d335140017759
Alberto <alberto.izquierdomo@gmail.com>
2020-12-07 04:11:01 +0100

    Edit data conditions before save it into database by handling different cases

--- AlbertoIzq-7.FestivalDatabaseWebsite/app.py
+++ AlbertoIzq-7.FestivalDatabaseWebsite/app.py
@@ -10,11 +10,14 @@ db = SQLAlchemy(app)
 message_email_en = "Seems like I've got something from that email address already"
 message_email_es = "Parece que alguien ya ha introducido datos con ese correo electrónico"
 
-message_age_en = "Your age cannot be less than when you first went to a festival, can it?"
-message_age_es = "Tu edad no puede ser menor que la que tenías cuando fuiste por primera vez a un festival, ¿no?"
-
 message_first_en = "If you enter your age when going to your first festival then number of festivals cannot be 0, can it?"
 message_first_es = "Si has introducido la edad con la que fuiste a tu primer festival entonces el número de festivales no puede ser 0, ¿no?"
+
+message_first2_en = "If number of festivals is different than 0 then you have to enter your age when going to your first festival, don't you?"
+message_first2_es = "Si el número de festivales es diferente a 0 entonces tienes que introducir la edad con la que fuiste a tu primer festival, ¿no?"
+
+message_age_en = "Your age cannot be less than when you first went to a festival, can it?"
+message_age_es = "Tu edad no puede ser menor que la que tenías cuando fuiste por primera vez a un festival, ¿no?"
 
 message_yes2021_en = "If you say you wanna go to a festival in 2021, then please enter its name"
 message_yes2021_es = "Si dices que quieres ir a un festival en 2021, entonces introduce el nombre por favor"
@@ -69,21 +72,30 @@ def en_success():
         gender = request.form["form_gender"]
         music_genre = request.form["form_music_genre"]
         festival_number = request.form["form_festival_number"]
-        festival_age = request.form["form_festival_age"]
+        if request.form["form_festival_age"] == "":
+            festival_age = None
+        else:
+            festival_age = request.form["form_festival_age"]
         festival_name = request.form["form_festival_name"]
         festival_music_genre = request.form["form_festival_music_genre"]
         yesno_2021 = request.form["form_yesno_2021"]
-        festival_name_2021 = request.form["form_festival_name_2021"]
+        if request.form["form_festival_name_2021"] == "":
+            festival_name_2021 = None
+        else:
+            festival_name_2021 = request.form["form_festival_name_2021"]
 
-        if festival_age != "" and int(age) < int(festival_age):
-            return render_template("en-home.html", text = message_age_en)
-        elif int(festival_number) == 0 and festival_age != "" and int(festival_age) != 0:
+        # Handling missentered data before saving it into database
+        if int(festival_number) == 0 and festival_age is not None:
             return render_template("en-home.html", text = message_first_en)
-        elif yesno_2021 == "yes" and festival_name_2021 == "":
+        if int(festival_number) != 0 and festival_age is None:
+            return render_template("en-home.html", text = message_first2_en)
+        if festival_age is not None and int(festival_age) > int(age):
+            return render_template("en-home.html", text = message_age_en)
+        if yesno_2021 == "yes" and festival_name_2021 is None:
             return render_template("en-home.html", text = message_yes2021_en)
-        elif yesno_2021 == "no" and festival_name_2021 != "":
+        if yesno_2021 == "no" and festival_name_2021 is not None:
             return render_template("en-home.html", text = message_no2021_en)
-        elif db.session.query(Data).filter(Data.email==email).count() == 0: # count() counts how many values satisfy this condition
+        if db.session.query(Data).filter(Data.email==email).count() == 0: # count() counts how many values satisfy this condition
             data = Data(email, age, gender, music_genre, festival_number, festival_age,
                         festival_name, festival_music_genre, yesno_2021, festival_name_2021)
             db.session.add(data) # You add the variable created before for the database to recognize the values
@@ -104,10 +116,19 @@ def en_success():
             c_max_num_fest = db.session.query(func.max(Data.festival_number)).scalar()
             c_avg_num_fest = db.session.query(func.avg(Data.festival_number)).scalar()
             c_avg_num_fest = round(c_avg_num_fest, 1)
-            c_min_age_first = db.session.query(func.min(Data.festival_age)).scalar()
-            c_max_age_first = db.session.query(func.max(Data.festival_age)).scalar()
-            c_avg_age_first = db.session.query(func.avg(Data.festival_age)).scalar()
-            c_avg_age_first = round(c_avg_age_first, 1)
+            if db.session.query(func.min(Data.festival_age)).scalar() is not None:
+                c_min_age_first = db.session.query(func.min(Data.festival_age)).scalar()
+            else:
+                c_min_age_first = None
+            if db.session.query(func.max(Data.festival_age)).scalar() is not None:
+                c_max_age_first = db.session.query(func.max(Data.festival_age)).scalar()
+            else:
+                c_max_age_first = None
+            if db.session.query(func.avg(Data.festival_age)).scalar() is not None:
+                c_avg_age_first = db.session.query(func.avg(Data.festival_age)).scalar()
+                c_avg_age_first = round(c_avg_age_first, 1)
+            else:
+                c_avg_age_first = None
             c_yes = db.session.query(Data).filter(Data.yesno_2021=="yes").count()
             c_yes = str(round(float(100 * c_yes / c_number_entries), 2))
             c_no = db.session.query(Data).filter(Data.yesno_2021=="no").count()
